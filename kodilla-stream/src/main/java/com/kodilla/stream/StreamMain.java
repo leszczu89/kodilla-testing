01199a4377d3099b296a897782c132085e5649a1
Paulina Leszczynska <paulinarudz@wp.pl>
2021-05-20 19:21:49 +0200

    7.1 Added stream module

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Locale;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        String string = "This is a beautiful day";
+        String string2 = "ABC";
+        String string3 = "This strings length is: ";
+        String string4 = "This fragment won't be shown, This string is a substring of a string.";
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify(string, String::toUpperCase));
+        System.out.println(poemBeautifier.beautify(string, stringTest -> string + " " + string2));
+        System.out.println(poemBeautifier.beautify(string3, stringTest -> string3 + string3.length()));
+        System.out.println(poemBeautifier.beautify(string4, stringTest -> string4.substring(30)));
+    }
+
+}
