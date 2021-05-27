3cc9551de3d759821562d3623dc7d5db1adf2eb2
Paulina Leszczynska <paulinarudz@wp.pl>
2021-05-27 14:44:40 +0200

    8.2 Added FirstChallenge in test package

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -5,8 +5,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
