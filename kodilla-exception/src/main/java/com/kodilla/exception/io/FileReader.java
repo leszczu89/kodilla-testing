fe9d782efa028c226262d7a06943bcda6cd3e7c1
Paulina Leszczynska <paulinarudz@wp.pl>
2021-05-31 17:18:46 +0200

    Throw and Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -2,8 +2,6 @@ package com.kodilla.exception.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,22 +9,31 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+//    public void readFile() throws FileReaderException {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("names.txt").getFile());
+//
+//        try {
+//            Stream<String> fileLines = Files.lines(Paths.get(file.getPath().replaceAll("%20", " ")));
+//            fileLines.forEach(System.out::println);
+//        } catch (IOException e) {
+//            throw new FileReaderException();
+//        } finally {
+//            System.out.println("I am gonna be here... always!");
+//        }
+//
+//        System.out.println(file.getPath().replaceAll("%20", " "));
+//    }
+    public void readFile(final String fileName) throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("names.txt").getFile());
-        Path path = Paths.get(file.getPath().replaceAll("%20", " "));
 
-        try {
-            Stream<String> fileLines = Files.lines(path);
+        try (Stream<String> fileLines = Files.lines(Path.of(classLoader.getResource(fileName).toURI()))) {
             fileLines.forEach(System.out::println);
-        } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+        } catch (Exception e) {
+            throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
-
-        System.out.println(file.getPath().replaceAll("%20", " "));
     }
-
 }
 
